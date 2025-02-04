fa9ccd434ceecbe9d31716201c56924898164f8d
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-02-04 14:33:48 +0200

    Created 'Team' by A2SV Hub

---
+++ 04-Feb-2025/Team 239914.py
@@ -0,0 +1,11 @@
+# Problem: Team - https://codeforces.com/contest/231/problem/A
+
+t = int(input())
+count = 0
+
+for _ in range(t):
+    a, b, c = map(int, input().split())
+    if a + b + c >= 2:
+        count += 1
+
+print(count)
