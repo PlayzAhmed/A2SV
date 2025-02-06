73bcecfc843e1edb45aef4b74209bdccdb958766
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-02-06 20:32:36 +0200

    Created 'Dragons' by A2SV Hub

---
+++ 06-Feb-2025/Dragons 247958.py
@@ -0,0 +1,27 @@
+# Problem: Dragons - https://codeforces.com/problemset/problem/230/A
+
+s, n = map(int, input().split())
+
+dragons = []
+die = False
+
+for _ in range(n):
+    d, b = map(int, input().split())
+    if s > d:
+        s += b
+    else:
+        dragons.append([d, b])
+
+dragons.sort()
+
+for d, b in dragons:
+    if s > d:
+        s += b
+    else:
+        die = True
+        break
+
+if die:
+    print("NO")
+else:
+    print("YES")
