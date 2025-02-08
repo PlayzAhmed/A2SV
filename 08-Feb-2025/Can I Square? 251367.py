417dd25b7754a5acbb7ff95404d1b7416fd59521
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-02-08 13:48:40 +0200

    Created 'Can I Square?' by A2SV Hub

---
+++ 08-Feb-2025/Can I Square? 251367.py
@@ -0,0 +1,17 @@
+# Problem: Can I Square? - https://codeforces.com/contest/1915/problem/C
+
+import math
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    sqr = sum(a)
+
+    side = int(math.sqrt(sqr))
+
+    if side*side == sqr:
+        print("YES")
+    else:
+        print("NO")
