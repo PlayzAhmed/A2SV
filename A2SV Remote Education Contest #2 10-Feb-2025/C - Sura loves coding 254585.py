263ad20a6150ab04d55a51c1880afed2fafe6bc0
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-02-10 22:03:02 +0200

    Created 'C - Sura loves coding' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 10-Feb-2025/C - Sura loves coding 254585.py
@@ -0,0 +1,26 @@
+# Problem: C - Sura loves coding - https://codeforces.com/gym/586622/problem/C
+
+n = int(input())
+s = input()
+res = [" "]*n
+j = 0
+k = n-1
+
+if n % 2 == 1:
+    for i in range(n-1, -1, -1):
+        if i % 2 == 0:
+            res[k] = s[i]
+            k -= 1
+        else:
+            res[j] = s[i]
+            j += 1
+else:
+    for i in range(n-1, -1, -1):
+        if i % 2 == 1:
+            res[k] = s[i]
+            k -= 1
+        else:
+            res[j] = s[i]
+            j += 1
+
+print("".join(res))
