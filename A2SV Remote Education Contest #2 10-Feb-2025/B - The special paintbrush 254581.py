b5f88a9606bad6a46de4e92e18bb2b37d783af63
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-02-10 22:02:33 +0200

    Created 'B - The special paintbrush' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 10-Feb-2025/B - The special paintbrush 254581.py
@@ -0,0 +1,18 @@
+# Problem: B - The special paintbrush - https://codeforces.com/gym/586622/problem/B
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    s = input()
+    
+
+    if (s[0] == 'B' and s[-1] == 'B') or 'W' not in s:
+        print(n)
+    else:
+        indices = []
+        for i in range(len(s)):
+            if s[i] == 'B':
+                indices.append(i)
+                
+        print(indices[len(indices)-1] - indices[0] + 1)
