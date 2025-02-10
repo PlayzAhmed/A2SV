620e1885bbd7ff6b962a7769bb065ddba0241ef9
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-02-10 22:03:24 +0200

    Created 'D - Mugisho and Rufeyda' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 10-Feb-2025/D - Mugisho and Rufeyda 254586.py
@@ -0,0 +1,17 @@
+# Problem: D - Mugisho and Rufeyda - https://codeforces.com/gym/586622/problem/D
+
+n, t = map(int, input().split())
+sq = t*t
+z = len(str(sq))
+
+if n == z:
+    print(sq)
+elif n == len(str(t)):
+    print(t)
+elif n > z:
+    while n > z:
+        sq *= 10
+        z = len(str(sq))
+    print(sq)
+else:
+    print(-1)
