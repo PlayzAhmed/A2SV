74073addee4d7a6b109655b633667aeeb3f398ef
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-02-13 22:51:43 +0200

    Created 'Runner up score' by A2SV Hub

---
+++ 13-Feb-2025/Runner up score 263935.py
@@ -0,0 +1,9 @@
+# Problem: Runner up score - https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    arr = list(set(arr))
+    arr.sort()
+    
+    print(arr[-2])
