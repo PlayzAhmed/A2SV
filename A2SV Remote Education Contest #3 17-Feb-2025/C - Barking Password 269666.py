57ccfbc9da4dab540179e18c6b2efe379d96fabe
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-02-17 21:08:36 +0200

    Created 'C - Barking Password' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/C - Barking Password 269666.py
@@ -0,0 +1,26 @@
+# Problem: C - Barking Password - https://codeforces.com/gym/588094/problem/C
+
+password = input()
+first = False
+second = False
+
+for _ in range(int(input())):
+    s = input()
+
+    if password == s:
+        print("YES")
+        exit()
+
+    if password[0] == s[1]:
+        first = True
+
+    if password[1] == s[0]:
+        second = True
+
+    if first and second:
+        break
+
+if first and second:
+    print("YES")
+else:
+    print("NO")
