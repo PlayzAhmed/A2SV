bdf3b1ce5700472d009d159d35fd73e7658486c6
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-02-17 21:18:10 +0200

    Created 'D - Adjacents, ewww !' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/D - Adjacents, ewww ! 269702.py
@@ -0,0 +1,36 @@
+# Problem: D - Adjacents, ewww ! - https://codeforces.com/gym/588094/problem/D
+
+for _ in range(int(input())):
+    n = int(input())
+
+    if n == 1:
+        print(1)
+    elif n == 2:
+        print(-1)
+    else:
+        matrix = []
+        for m in range(n):
+            matrix.append([0]*n)
+        ecol = 0
+        erow = 0
+        ocol = (((n*n)//2) % n)
+        orow = ((n*n)//2) // n
+
+        for i in range(1, n*n + 1):
+            if i % 2 == 0:
+                matrix[erow][ecol] = i
+                if erow + 1 < n and ecol+1 == n:
+                    erow += 1
+                    ecol = 0
+                else:
+                    ecol += 1
+            else:
+                matrix[orow][ocol] = i
+                if orow + 1 < n and ocol + 1 == n:
+                    orow += 1
+                    ocol = 0
+                else:
+                    ocol += 1
+        
+        for row in matrix:
+            print(*row)
