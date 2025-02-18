b7f0b24be9a20e8beed68c4c6c21c4d998499581
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-02-18 16:35:48 +0200

    Created 'Chat Order' by A2SV Hub

---
+++ 18-Feb-2025/Chat Order 272093.py
@@ -0,0 +1,17 @@
+# Problem: Chat Order - https://codeforces.com/problemset/problem/637/B
+
+n = int(input())
+messages = []
+
+for _ in range(n):
+    s = input()
+    messages.append(s)
+
+messages = messages[::-1]
+
+his = {}
+
+for m in messages:
+    if m not in his:
+        print(m)
+        his[m] = 1
