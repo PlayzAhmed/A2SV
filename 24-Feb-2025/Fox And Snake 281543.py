36ae74e86077e5a4aaed2b2e0bdf4508da1f6bfb
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-02-24 12:41:00 +0200

    Created 'Fox And Snake' by A2SV Hub

---
+++ 24-Feb-2025/Fox And Snake 281543.py
@@ -0,0 +1,13 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+row, col = map(int, input().split())
+
+for r in range(row):
+    if r == 0 or r == row - 1 or r % 2 == 0:
+        print("#" * col, end="")
+    elif r % 4 == 1:
+        print("." * (col-1) + "#", end="")
+    else:
+        print("#" + "." * (col-1), end="")
+
+    print()
