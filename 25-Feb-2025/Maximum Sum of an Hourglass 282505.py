851cafd29ff39e4400957c788c0ca49c9f3bc819
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-02-25 16:21:12 +0200

    Created 'Maximum Sum of an Hourglass' by A2SV Hub

---
+++ 25-Feb-2025/Maximum Sum of an Hourglass 282505.py
@@ -0,0 +1,21 @@
+# Problem: Maximum Sum of an Hourglass - https://leetcode.com/problems/maximum-sum-of-an-hourglass/
+
+class Solution(object):
+    def maxSum(self, grid):
+        rows = len(grid)
+        cols = len(grid[0])
+        preSum = [[0]*(cols+1) for _ in range(rows+1)]
+        maxSum = -1
+
+
+        for row in range(1, rows+1): 
+            for col in range(1, cols+1):
+                preSum[row][col] = grid[row-1][col-1] + preSum[row-1][col] + preSum[row][col-1] - preSum[row-1][col-1]
+
+        for row in range(rows-2):
+            for col in range(cols-2):
+                subMatrix = preSum[row+3][col+3] - preSum[row][col+3] - preSum[row+3][col] + preSum[row][col]
+                hourglass = subMatrix - grid[row+1][col] - grid[row+1][col+2]
+                maxSum = max(maxSum, hourglass)
+
+        return maxSum
