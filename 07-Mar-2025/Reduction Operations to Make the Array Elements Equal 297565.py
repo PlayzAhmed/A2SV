e63ec2c48543fbdd012be3f420a11262e0d1a182
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-03-07 22:00:20 +0200

    Created 'Reduction Operations to Make the Array Elements Equal' by A2SV Hub

---
+++ 07-Mar-2025/Reduction Operations to Make the Array Elements Equal 297565.py
@@ -0,0 +1,12 @@
+# Problem: Reduction Operations to Make the Array Elements Equal - https://leetcode.com/problems/reduction-operations-to-make-the-array-elements-equal/
+
+class Solution(object):
+    def reductionOperations(self, nums):
+        freq = Counter(nums)
+        nums = sorted(list(set(nums)))
+        op = 0
+
+        for i in range(len(nums)-1, 0, -1):
+            op += i * freq[nums[i]]
+
+        return op
