b2e26cc9aa3949545f4596dcf4b610165f2896b6
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-03-07 21:14:06 +0200

    Created 'The Maximum Number' by A2SV Hub

---
+++ 07-Mar-2025/The Maximum Number 297474.py
@@ -0,0 +1,11 @@
+# Problem: The Maximum Number - https://leetcode.com/problems/third-maximum-number/description/
+
+class Solution(object):
+    def thirdMax(self, nums):
+        freq = []
+
+        for num in nums:
+            if num not in freq:
+                freq.append(num)
+        freq.sort(reverse=True)
+        return freq[2] if len(freq) >= 3 else max(freq)
