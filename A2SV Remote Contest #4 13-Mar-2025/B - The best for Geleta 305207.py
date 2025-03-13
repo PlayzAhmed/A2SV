5da78450bdc0c3a11556fbee6c120d43d55f5c64
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-03-13 17:33:54 +0200

    Created 'B - The best for Geleta' by A2SV Hub

---
+++ A2SV Remote Contest #4 13-Mar-2025/B - The best for Geleta 305207.py
@@ -0,0 +1,25 @@
+# Problem: B - The best for Geleta - https://codeforces.com/gym/590201/problem/B
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    res, mx = [0]*m, max(a)
+    
+    for i in range(m):
+        op, left, right = map(str, input().split())
+        left = int(left)
+        right = int(right)
+
+        if left <= mx <= right:
+            if op == '+':
+                mx += 1
+                res[i] = mx
+            else:
+                mx -= 1
+                res[i] = mx
+            
+            continue
+        
+        res[i] = mx
+        
+    print(*res)
