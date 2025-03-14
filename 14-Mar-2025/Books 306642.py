e605a302d3c6d024ffcdcaf33830d8a41d67b691
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-03-14 16:01:05 +0200

    Created 'Books' by A2SV Hub

---
+++ 14-Mar-2025/Books 306642.py
@@ -0,0 +1,25 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n, t = map(int, input().split())
+a = list(map(int, input().split()))
+
+i = 0 
+j = 0
+b = 0
+res = 0
+
+while j < n:
+    if t - a[j] >= 0:
+        t -= a[j]
+        j += 1
+        b += 1
+    else:
+        t += a[i]
+        i += 1
+        res = max(res, b)
+        b -= 1
+
+if t >= 0:
+    res = max(res, b)
+
+print(res)
