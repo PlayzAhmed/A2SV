89b213bc308caef2c3757490d76d3e7e7b9b6ede
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-03-17 17:51:30 +0200

    Created 'D - The Snap of Fury' by A2SV Hub

---
+++ A2SV Remote Contest #7 17-Mar-2025/D - The Snap of Fury 309419.py
@@ -0,0 +1,25 @@
+# Problem: D - The Snap of Fury - https://codeforces.com/gym/596004/problem/D
+
+n = int(input())
+a = list(map(int, input().split()))
+
+if a[-1] >= n:
+    print(1)
+    exit()
+
+diff = [0] * (n + 1)
+
+for i in range(1, n):
+    if a[i] > 0:
+        left = max(0, i - a[i])
+        diff[left] += 1 
+        diff[i] -= 1
+
+kills = 0
+current = 0
+for i in range(n):
+    current += diff[i]
+    if current > 0:
+        kills += 1
+
+print(n - kills)
