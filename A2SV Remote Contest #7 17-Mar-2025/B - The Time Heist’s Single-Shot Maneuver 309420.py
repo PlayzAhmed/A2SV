f868adc247f6b3923d48847186cbcfae59c10b83
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-03-17 17:51:39 +0200

    Created 'B - The Time Heist’s Single-Shot Maneuver' by A2SV Hub

---
+++ A2SV Remote Contest #7 17-Mar-2025/B - The Time Heist’s Single-Shot Maneuver 309420.py
@@ -0,0 +1,20 @@
+# Problem: B - The Time Heist’s Single-Shot Maneuver - https://codeforces.com/gym/596004/problem/B
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = int(input())
+
+    if n == 1:
+        print("YES")
+    else:
+        a[0] = min(a[0], b-a[0])
+        for i in range(1, n):
+            t = b - a[i] 
+            if t >= a[i-1]:
+                if a[i] >= a[i-1]:
+                    a[i] = min(a[i], t)
+                else:
+                    a[i] = t
+
+        print("YES" if sorted(a) == a else "NO")
