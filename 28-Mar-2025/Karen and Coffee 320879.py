7330821088867ee81c6abe1224437be58cb53a2a
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-03-28 16:25:26 +0200

    Created 'Karen and Coffee' by A2SV Hub

---
+++ 28-Mar-2025/Karen and Coffee 320879.py
@@ -0,0 +1,27 @@
+# Problem: Karen and Coffee - https://codeforces.com/contest/816/problem/B
+
+n, k, q = map(int, input().split())
+recipes = []
+questions = []
+arr1 = [0] * 200005
+arr2 = [0] * 200005
+mn = float("inf")
+mx = float("-inf")
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    recipes.append([a, b])
+    mn = min(mn, a)
+    mx = max(mx, b)
+    arr1[a] += 1
+    arr1[b+1] -= 1
+
+for i in range(1, 200005):
+    arr1[i] += arr1[i-1]
+    arr2[i] += arr2[i-1] + (arr1[i] >= k)
+
+
+
+for i in range(q):
+    a, b = map(int, input().split())
+    print(arr2[b]-arr2[a-1])
