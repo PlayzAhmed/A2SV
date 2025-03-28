e6551ed7e5f77a6c7e8ce54dc1650037e2ba41c5
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-03-28 17:01:20 +0200

    Created 'C - Happy Partitioning' by A2SV Hub

---
+++ A2SV Remote Contest #4 28-Mar-2025/C - Happy Partitioning 320919.py
@@ -0,0 +1,37 @@
+# Problem: C - Happy Partitioning - https://codeforces.com/gym/590201/problem/C
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().strip()))
+
+    prefix_zeros = [0] * (n + 1)
+    suffix_ones = [0] * (n + 1)
+
+    for i in range(n):
+        prefix_zeros[i + 1] = prefix_zeros[i] + (a[i] == 0)
+
+    for i in range(n - 1, -1, -1):
+        suffix_ones[i] = suffix_ones[i + 1] + (a[i] == 1)
+
+    best_pos = -1
+    best_dist = float('inf')
+
+    for i in range(n + 1): 
+        left_zeros = prefix_zeros[i]
+        left_len = i
+        right_ones = suffix_ones[i]
+        right_len = n - i
+
+        if left_zeros >= left_len / 2 and right_ones >= right_len / 2:
+            dist = abs(n - 2 * i)
+            if dist < best_dist:
+                best_dist = dist
+                best_pos = i
+
+    if best_pos != -1:
+        print(best_pos)
+    else:
+        if a.count(0) > n / 2:
+            print(n)
+        else:
+            print(0)
