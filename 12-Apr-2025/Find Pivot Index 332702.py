2b0a864e373a038550fa0bdc990669d21a6bbbf6
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-04-12 23:08:08 +0200

    Created 'Find Pivot Index' by A2SV Hub

---
+++ 12-Apr-2025/Find Pivot Index 332702.py
@@ -0,0 +1,16 @@
+# Problem: Find Pivot Index - https://leetcode.com/problems/find-pivot-index/description/
+
+class Solution(object):
+    def pivotIndex(self, nums):
+        n = len(nums)
+        for i in range(1, n):
+            nums[i] += nums[i-1]
+        if nums[n-1] - nums[0] == 0:
+            return 0
+            
+        for l in range(n-1):
+            if nums[l] == nums[n-1] - nums[l+1]:
+                return l+1
+
+        return -1
+        
