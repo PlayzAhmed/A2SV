2a8eea2df570e313a1890c7663ee8b81b54457ad
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-04-22 20:04:20 +0200

    Created 'Removing Stars From a String' by A2SV Hub

---
+++ 22-Apr-2025/Removing Stars From a String 338840.py
@@ -0,0 +1,15 @@
+# Problem: Removing Stars From a String - https://leetcode.com/problems/removing-stars-from-a-string/description/
+
+class Solution(object):
+    def removeStars(self, s):
+        stack = []
+
+        for x in s:
+            if x == "*" and stack:
+                stack.pop()
+            elif x != "*":
+                stack.append(x)
+
+        return "".join(stack)
+
+        
