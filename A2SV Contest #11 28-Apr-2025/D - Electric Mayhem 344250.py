acd24d3a4df31e1e059fc5218790bde15f2272fc
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-04-28 21:49:38 +0300

    Created 'D - Electric Mayhem' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/D - Electric Mayhem 344250.py
@@ -0,0 +1,12 @@
+# Problem: D - Electric Mayhem - https://codeforces.com/gym/605795/problem/D
+
+s = input()
+stack = []
+
+for ch in s:
+    if stack and stack[-1] == ch:
+        stack.pop()
+    else:
+        stack.append(ch)
+
+print("No" if stack else "Yes")
