ed07b0c75b1916d34f124824b2247c87f7c2f328
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-04-29 20:39:38 +0300

    Created 'Twin Permutations' by A2SV Hub

---
+++ 29-Apr-2025/Twin Permutations 344852.py
@@ -0,0 +1,11 @@
+# Problem: Twin Permutations - https://codeforces.com/problemset/problem/1831/A
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    res = []
+
+    for x in a:
+        res.append(n-x+1)
+
+    print(*res)
