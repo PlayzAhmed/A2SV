4dea235f1542f417a529af06c3a04f3e594d7410
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-06-02 19:58:40 +0300

    Created 'Merge Interval' by A2SV Hub

---
+++ 02-Jun-2025/Merge Interval 362837.py
@@ -0,0 +1,18 @@
+# Problem: Merge Interval - https://leetcode.com/problems/merge-intervals/
+
+class Solution(object):
+    def merge(self, intervals):
+        n = len(intervals)
+
+        intervals = sorted(intervals, key=min)
+
+        res = [intervals[0]]
+
+        for i in range(1, n):
+            if res[-1][1] >= intervals[i][0]:
+                res[-1][1] = max(res[-1][1], intervals[i][1])
+            else:
+                res.append(intervals[i])
+
+        
+        return res
