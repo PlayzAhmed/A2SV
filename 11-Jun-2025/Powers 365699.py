91f124ec07274c2ba053dd3e93c99fb5e86f22b1
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-06-11 22:21:21 +0300

    Created 'Powers' by A2SV Hub

---
+++ 11-Jun-2025/Powers 365699.py
@@ -0,0 +1,6 @@
+# Problem: Powers - https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+
+If __name__ == "__main__":
+    n = int(input())
+    for x in range(n):
+        print(x ** 2)
