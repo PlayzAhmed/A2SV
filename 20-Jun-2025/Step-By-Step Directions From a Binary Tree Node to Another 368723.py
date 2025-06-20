b6a9d07c9e71e39f46f4c7dc8a1473f9c850703d
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-06-20 22:51:14 +0300

    Created 'Step-By-Step Directions From a Binary Tree Node to Another' by A2SV Hub

---
+++ 20-Jun-2025/Step-By-Step Directions From a Binary Tree Node to Another 368723.py
@@ -0,0 +1,37 @@
+# Problem: Step-By-Step Directions From a Binary Tree Node to Another - https://leetcode.com/problems/step-by-step-directions-from-a-binary-tree-node-to-another/
+
+class Solution(object):
+    def getDirections(self, root, startValue, destValue):
+        
+        def dfs(node, target, path=deque()):
+            if not node: return False
+            if node.val == target: return path
+            
+            path.append("L")
+            ans = dfs(node.left, target, path)
+            if ans:
+                return ans
+            path.pop()
+
+            path.append("R")
+            ans = dfs(node.right, target, path)
+            if ans:
+                return ans
+            path.pop()
+
+        first = dfs(root, startValue)
+        sec = dfs(root, destValue, deque())
+        res = ""
+
+        while first and sec and first[0] == sec[0]:
+            first.popleft()
+            sec.popleft()
+
+        while first:
+            first.pop()
+            res += "U"
+
+        while sec:
+            res += sec.popleft()
+
+        return res
