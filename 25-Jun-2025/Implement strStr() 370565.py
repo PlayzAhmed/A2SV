c1186b8af7d0b4c3c17e30c62f849be6a67ffb31
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-06-25 23:51:17 +0300

    Created 'Implement strStr()' by A2SV Hub

---
+++ 25-Jun-2025/Implement strStr() 370565.py
@@ -0,0 +1,6 @@
+# Problem: Implement strStr() - https://leetcode.com/problems/implement-strstr/
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        return haystack.find(needle)
+        
