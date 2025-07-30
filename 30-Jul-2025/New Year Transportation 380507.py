9f5b6c818fde49fb526f10d6bb266e5e64aaeb8e
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-07-30 08:04:22 +0300

    Created 'New Year Transportation' by A2SV Hub

---
+++ 30-Jul-2025/New Year Transportation 380507.py
@@ -0,0 +1,11 @@
+# Problem: New Year Transportation - https://codeforces.com/problemset/problem/500/A
+
+n, target = map(int, input().split())
+a = list(map(int, input().split()))
+i = 0
+
+while i < target-1:
+    i += a[i]
+
+
+print("YES" if i == target-1 else "NO")
