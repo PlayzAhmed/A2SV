1a70aa264a8d02829b0fcd4e9def6207e90cd43d
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-08-02 23:34:08 +0300

    Created 'Coin Change' by A2SV Hub

---
+++ 02-Aug-2025/Coin Change 381458.py
@@ -0,0 +1,13 @@
+# Problem: Coin Change - https://leetcode.com/problems/coin-change/
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        dp = [float('inf')] * (amount+1)
+        dp[0] = 0
+
+        for a in range(1, amount+1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c])
+
+        return dp[amount] if dp[amount] != float('inf') else -1
