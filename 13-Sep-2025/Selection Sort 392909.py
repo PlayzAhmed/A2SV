fce84aacface8d9f16bfce236d5aa17556fe0892
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-09-13 09:22:55 +0300

    Created 'Selection Sort' by A2SV Hub

---
+++ 13-Sep-2025/Selection Sort 392909.py
@@ -0,0 +1,12 @@
+# Problem: Selection Sort - https://practice.geeksforgeeks.org/problems/selection-sort/1
+
+class Solution: 
+    def selectionSort(self, arr):
+        
+        for i in range(len(arr)):
+            for j in range(i+1, len(arr)):
+                if arr[i] > arr[j]:
+                    arr[i], arr[j] = arr[j], arr[i]
+                    
+            
+        return arr
