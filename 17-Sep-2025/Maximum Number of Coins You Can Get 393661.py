4942aec1efa19686edebf280475a1f800d0ef92f
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-09-17 13:50:10 +0300

    Created 'Maximum Number of Coins You Can Get' by A2SV Hub

---
+++ 17-Sep-2025/Maximum Number of Coins You Can Get 393661.py
@@ -0,0 +1,14 @@
+# Problem: Maximum Number of Coins You Can Get - https://leetcode.com/problems/maximum-number-of-coins-you-can-get/
+
+class Solution(object):
+    def maxCoins(self, piles):
+        piles.sort()
+        n = len(piles)
+        piles = piles[n//3:]
+
+        s = 0
+
+        for i in range(0, n-(n//3), 2):
+            s += piles[i]
+
+        return s
