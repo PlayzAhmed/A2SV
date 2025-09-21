3a350adedf86d0035b95ae92538ec8fedba68524
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-09-21 14:09:54 +0300

    Created 'Odd Divisor' by A2SV Hub

---
+++ 21-Sep-2025/Odd Divisor 394616.py
@@ -0,0 +1,18 @@
+# Problem: Odd Divisor - https://codeforces.com/problemset/problem/1475/A
+
+from math import sqrt
+
+for _ in range(int(input())):
+    n = int(input())
+    f = False
+
+    if n % 2:
+        print("YES")
+        continue
+    while n > 1:
+        n //= 2
+        if n % 2 and n > 1:
+            f = True
+            break
+
+    print("YES" if f else "NO")
