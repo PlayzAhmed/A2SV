3998602ca5a4d71de44f4fac2401209e8555c545
Ahmed Mohammed <78406761+PlayzAhmed@users.noreply.github.com>
2025-10-13 00:45:08 +0300

    Created 'Single Number II' by A2SV Hub

---
+++ 12-Oct-2025/Single Number II 402475.py
@@ -0,0 +1,12 @@
+# Problem: Single Number II - https://leetcode.com/problems/single-number-ii/
+
+class Solution(object):
+    def singleNumber(self, nums):
+        ones = 0
+        twos = 0
+
+        for num in nums:
+            ones = (ones ^ num) & ~twos
+            twos = (twos ^ num) & ~ones
+
+        return ones
